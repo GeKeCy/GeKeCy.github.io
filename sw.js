9acad636a67d812fe7bdb6837d000b8fdf15f5e4
GeKeCy创始人 <forest2580@qq.com>
2026-05-31 15:10:37 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'gekecy-v1.5.335';
+
+const CORE_ASSETS = [
+  './',
+  './index(1.5.335-测).html',
+  './manifest.json',
+  './1774103627050.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(CORE_ASSETS);
+    }).catch(err => {
+      console.log('Cache install error:', err);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(networkRes => {
+        if (networkRes && networkRes.ok) {
+          const clone = networkRes.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(e.request, clone);
+          });
+        }
+        return networkRes;
+      }).catch(() => {
+        return cached;
+      });
+
+      return cached || fetchPromise;
+    })
+  );
+});
